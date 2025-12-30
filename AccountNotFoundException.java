ec912578f9aa23b121c6511a9645c8686c58bb3a
picasso653 <xquisit31@gmail.com>
2025-12-30 08:22:30 +0000

    finally

--- AccountNotFoundException.java
+++
@@ -1,9 +0,0 @@
-
-
-public class AccountNotFoundException extends Exception {
-    public AccountNotFoundException(String message) {
-        super(message);
-    }
-}
-    
-
--- Timer.java
+++
@@ -1,9 +0,0 @@
-public class Timer {
-    public static void delayTimer(){
-        try {
-            Thread.sleep(3000);
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
-        }
-    }
-}
